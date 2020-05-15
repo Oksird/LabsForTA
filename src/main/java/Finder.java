7ad0a253d996c2b4666393797d6eff0892fe9f69
Maxim Muzychenko <m.muz.al3105@gmail.com>
2020-05-16 01:41:31 +0300

    Add comments

--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -9,17 +9,20 @@ public class Finder {
 
 
     public static void main(String[] args) {
-
-        int count = 1000000;                   // Count of elements
-        int result = 0;
-        long elapsedTime = 0;
-        int heightToCompare = 400;
+        Random random = new Random();
+        //Limits of fields
         int maxX = 300000;
         int maxY = 300000;
         int maxHeight = 828;
-
-        Random random = new Random();
-
+        // Structure of object
+        int x= random.nextInt(maxX);
+        int y = random.nextInt(maxY);
+        int height = random.nextInt(maxHeight);
+        // Count of elements
+        int count = 1000000;
+        int result = 0;
+        long elapsedTime = 0;
+        int heightToCompare = 400;
 
         try {
             File file = new File(FILE_PATH);
@@ -31,13 +34,13 @@ public class Finder {
             String[] header = { "X", "Y", "Height" };
             writer.writeNext(header);
 
-            String[] data = new String[3];                                  //1
+            String[] data = new String[3];
             long start = System.currentTimeMillis();
-            for (int i = 0; i <count ; i++) {                             //n
-                data[0]= String.valueOf(random.nextInt(maxX));     //1
-                data[1] = String.valueOf(random.nextInt(maxY));    //1
-                data[2] = String.valueOf(random.nextInt(maxHeight));       //1
-                if(Integer.parseInt(data[2])>heightToCompare)                           //1
+            for (int i = 0; i <count ; i++) {                                //n
+                data[0]= String.valueOf(x);                                  //1
+                data[1] = String.valueOf(y);                                 //1
+                data[2] = String.valueOf(height);                            //1
+                if(Integer.parseInt(data[2])>heightToCompare)                //1
                     result++;                                               //1
                 writer.writeNext(data);                                     //1
 
